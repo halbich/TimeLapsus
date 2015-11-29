ac471ff742f6d59bb5570b15725ea7c9cf33daf6
Pavel Halbich <halbich.p@hotmail.com>
2015-11-29 17:41:26 +0100

    Animace, Základ videa, Základ všech obrazovek

---
+++ TimeLapsus/Assets/Scripts/RespawnPointScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnPointScript : MonoBehaviour
+{
+
+    public EnumLevel LevelName;
+
+    public Facing Direction;
+
+    public RespawnPoint GetPoint(GameObject obj)
+    {
+        return new RespawnPoint { LevelName = this.LevelName, Direction = this.Direction, StartPoint = obj.transform.position };
+    }
+
+}
+
+public struct RespawnPoint
+{
+
+    public EnumLevel LevelName;
+
+    public Vector3 StartPoint;
+
+    public Facing Direction;
+
+    public static bool operator ==(RespawnPoint a, RespawnPoint b)
+    {
+        // Return true if the fields match:
+        return a.LevelName == b.LevelName && a.StartPoint == b.StartPoint && a.Direction == b.Direction;
+    }
+
+    public static bool operator !=(RespawnPoint a, RespawnPoint b)
+    {
+        return !(a == b);
+    }
+}
+
---
+++ TimeLapsus/Assets/Video/VideoPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoPlayer : MonoBehaviour {
+
+    public MovieTexture movTexture;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+	}
+	
+
+}
