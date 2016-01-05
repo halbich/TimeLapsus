3b9fe6f161167a450e8170657d60a9591167dd25
Pavel Halbich <halbich.p@hotmail.com>
2016-01-05 23:38:13 +0100

    průchod hrou

---
+++ TimeLapsus/Assets/Scripts/InventoryManagement/UseMoneyOnBanker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class UseMoneyOnBanker : ItemUseOnScript
+{
+    public string SetAccountHasMoneyVarName;
+    public string UseMoneyDialog;
+
+    public override void Use()
+    {
+        if (ObjectPoint != null)
+
+            Controller.PlayerController.MoveTo(ObjectPoint.StartPoint, () =>
+            {
+                Controller.PlayerController.SetNewFacing(ObjectPoint.Direction);
+                QuestController.Instance.GetCurrent().SetBoolean(SetAccountHasMoneyVarName);
+                var dialogController = DialogController.Instance;
+                dialogController.ShowDialog(dialogController.GetDialog(UseMoneyDialog), GetComponent<DialogActor>().Avatar);
+                base.Use();
+            });
+        else Debug.LogError("No object point defined!");
+    }
+}
+
---
+++ TimeLapsus/Assets/Scripts/InventoryManagement/UseRobotOnPotter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class UseRobotOnPotter : ItemUseOnScript
+{
+    public string PotterIsDeadVarName;
+    public string AfterPotterDeadDialog;
+
+    public override void Use()
+    {
+        if (ObjectPoint != null)
+
+            Controller.PlayerController.MoveTo(ObjectPoint.StartPoint, () =>
+            {
+                Controller.PlayerController.SetNewFacing(ObjectPoint.Direction);
+                QuestController.Instance.GetCurrent().SetBoolean(PotterIsDeadVarName);
+                var dialogController = DialogController.Instance;
+                dialogController.ShowDialog(dialogController.GetDialog(AfterPotterDeadDialog));
+                base.Use();
+                Destroy(gameObject);
+            });
+        else Debug.LogError("No object point defined!");
+    }
+}
+
