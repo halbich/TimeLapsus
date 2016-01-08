672fceb1f1fedb74acd4fa4ebe52023dbc8b7dbe
Pavel Halbich <halbich.p@hotmail.com>
2016-01-09 00:03:09 +0100

    hotfix chyb

--- TimeLapsus/Assets/Scripts/Dialogs/DialogImplementations/GameEndTrigger.cs
+++ TimeLapsus/Assets/Scripts/Dialogs/DialogImplementations/GameEndTrigger.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GameEndTrigger : ScriptWithController
 {
     private void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(nextLevel());
+    }
+
+    IEnumerator nextLevel()
+    {
+        yield return new WaitForSeconds(2);
+
         Controller.ChangeScene(EnumLevel.About);
     }
 }
