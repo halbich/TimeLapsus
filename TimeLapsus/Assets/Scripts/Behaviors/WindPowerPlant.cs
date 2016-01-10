8e417f6710a60ce6edd9f4fcc48ce8d764136be7
Pavel Halbich <halbich.p@hotmail.com>
2016-01-10 21:02:47 +0100

    dodělávky

---
+++ TimeLapsus/Assets/Scripts/Behaviors/WindPowerPlant.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vrtule : MonoBehaviour
+{
+
+    public float Rate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        transform.Rotate(Vector3.forward, Rate * Time.deltaTime);
+	}
+}
