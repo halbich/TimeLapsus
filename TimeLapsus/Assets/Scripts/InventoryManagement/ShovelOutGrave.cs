bede218153a339d09f268e8f665b3ab247745415
Pavel Halbich <halbich.p@hotmail.com>
2016-01-11 12:41:52 +0100

    opravy chyb, úpravy

--- TimeLapsus/Assets/Scripts/InventoryManagement/ShovelOutGrave.cs
+++ TimeLapsus/Assets/Scripts/InventoryManagement/ShovelOutGrave.cs
@@ -1,12 +1,27 @@
-﻿public class ShovelOutGrave : ItemUseOnScript
+﻿using UnityEngine;
+
+public class ShovelOutGrave : ItemUseOnScript
 {
     public string ShoveledGraveKeyName;
 
     public override void Use()
     {
-        base.Use();
-        QuestController.Instance.GetCurrent().SetBoolean(ShoveledGraveKeyName);
-        var c = GetComponent<GravePresentController>();
-        c.ActionOccured();
+        if (ObjectPoint == null)
+        {
+            Debug.LogError("No object point defined!");
+            return;
+        }
+
+
+        Controller.PlayerController.MoveTo(ObjectPoint.StartPoint, () =>
+        {
+            Controller.PlayerController.SetNewFacing(ObjectPoint.Direction);
+            base.Use();
+            QuestController.Instance.GetCurrent().SetBoolean(ShoveledGraveKeyName);
+            var c = GetComponent<GravePresentController>();
+            c.ActionOccured();
+        });
+
+       
     }
 }
