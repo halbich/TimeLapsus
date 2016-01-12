54802ec5f9d740169051425e54adfa42021c1675
Pavel Halbich <halbich.p@hotmail.com>
2016-01-12 19:18:02 +0100

    úprava animace boje

--- TimeLapsus/Assets/Scripts/InventoryManagement/UseRobotOnPotter.cs
+++ TimeLapsus/Assets/Scripts/InventoryManagement/UseRobotOnPotter.cs
@@ -13,6 +13,8 @@ public class UseRobotOnPotter : ItemUseOnScript
     public ItemPointScript AnimEndPoint;
     public GameObject Hider;
 
+    public float WaitForRobot = 0.5f;
+
     protected override void Start()
     {
         base.Start();
@@ -46,68 +48,30 @@ public class UseRobotOnPotter : ItemUseOnScript
 
     IEnumerator animate()
     {
-        base.Use();
+        //base.Use();
+
+        Controller.PlayerCharacter.GetComponent<Animator>().SetTrigger("Throw");
         R2D2.SetActive(true);
-
         var animator = R2D2.GetComponent<Animator>();
+        yield return new WaitForSeconds(WaitForRobot);
+        animator.enabled = true;
 
-        animator.SetTrigger("Open");
-        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Open"));
+    }
 
-        yield return new WaitForSeconds(1);
-        var EPSILON = 0.1f;
-        var moveSpeed = 0.5f;
-        var endPoint = AnimEndPoint.GetPoint(gameObject.transform.position.z).StartPoint;
-        var originalPoint = R2D2.transform.position;
 
-        foreach (var p in MoveToPoint(originalPoint, endPoint, EPSILON, moveSpeed))
-            yield return p;
-
-        yield return new WaitForSeconds(2);
-
+    internal void KillPotter()
+    {
         GetComponent<SpriteRenderer>().enabled = false;
-        GetComponent<PolygonCollider2D>().enabled = false;
-        Hider.SetActive(false);
-
         QuestController.Instance.GetCurrent().SetBoolean(PotterIsDeadVarName);
+    }
+
+    internal void ShowFinalDialog()
+    {
         var dialogController = DialogController.Instance;
         dialogController.ShowDialog(dialogController.GetDialog(AfterPotterDeadDialog));
 
-        R2D2.GetComponent<SpriteRenderer>().flipX = true;
-        yield return new WaitForSeconds(0.5f);
-
-        foreach (var p in MoveToPoint(endPoint, originalPoint, EPSILON, moveSpeed, false))
-            yield return p;
-
-        animator.SetTrigger("Close");
-        yield return new WaitForSeconds(1);
         R2D2.SetActive(false);
-        Controller.AddInventoryItem(EnumItemID.Robot);
 
         Destroy(gameObject);
-    }
-
-    private IEnumerable MoveToPoint(Vector3 originalPoint, Vector3 endPoint, float EPSILON, float moveSpeed, bool triggerHider = true)
-    {
-        var remainig = Vector3.Distance(originalPoint, endPoint);
-        var direction = endPoint - R2D2.transform.position;
-        var isApproaching = true;
-        while (remainig >= EPSILON && isApproaching)
-        {
-            if(triggerHider && remainig <= 5f)
-                Hider.SetActive(true);
-
-            Debug.Log("move");
-            var newPoint = R2D2.transform.position + direction * moveSpeed * Time.deltaTime;
-            var newRemaining = Vector3.Distance(endPoint, newPoint);
-            isApproaching = newRemaining < remainig;
-            Debug.LogFormat("{0}, {1}, {2}", isApproaching, remainig, newRemaining);
-            if (!isApproaching)
-                break;
-
-            R2D2.transform.position = newPoint;
-            remainig = newRemaining;
-            yield return null;
-        }
     }
 }
