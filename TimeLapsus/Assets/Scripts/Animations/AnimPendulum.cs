6cdfd40312dbe082c08d4906d70f3addcd4272bd
Pavel Halbich <halbich.p@hotmail.com>
2016-01-15 00:24:30 +0100

    dodělávky

--- TimeLapsus/Assets/Scripts/Animations/AnimPendulum.cs
+++ TimeLapsus/Assets/Scripts/Animations/AnimPendulum.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class AnimPendulum : MonoBehaviour
 {
     public float Angle = 5f;
+    public float StartTime = 0f;
     private float fTimer;
     private Vector3 v3T = Vector3.zero;
 
 
     void Update()
     {
-        float f = (Mathf.Sin(fTimer * Mathf.PI - Mathf.PI / 2.0f) + 1.0f) / 2.0f;
+        float f = (Mathf.Sin( (StartTime +fTimer) * Mathf.PI - Mathf.PI / 2.0f) + 1.0f) / 2.0f;
 
         v3T.Set(0.0f, 0.0f, Mathf.Lerp(Angle, -Angle, f));
         transform.eulerAngles = v3T;
